0673827ad469d9647d775358af90c621c826c1dc
Carl Schroedl <carlschroedl@gmail.com>
2017-07-01 11:32:52 -0500

    Brought all classes into PSR2 compliance via phpcbf

--- src/Candidate.php
+++ src/Candidate.php
@@ -1,28 +1,29 @@
 <?php
 namespace PivotLibre\Tideman;
 
-class Candidate 
+class Candidate
 {
-	private $id;
-	private $name;
-	
-	public function __construct($id, $name) {
-		$this->id = $id;
-		$this->name = $name;
-	}
-	
-	public function getId()
-	{
-		return $this->id;
-	}
-	
-	public function getName()
-	{
-		return $this->name;
-	}
-	
-	public function __toString()
-	{
-		return $this->name + "(" + $this->id + ")";
-	}
+    private $id;
+    private $name;
+    
+    public function __construct($id, $name) 
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+    
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    public function getName()
+    {
+        return $this->name;
+    }
+    
+    public function __toString()
+    {
+        return $this->name + "(" + $this->id + ")";
+    }
 }
