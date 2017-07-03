d3e5281c099d5ff1bc6c3e7e60a8b5bb0b48e0ba
Carl Schroedl <carlschroedl@gmail.com>
2017-07-02 20:44:08 -0500

    Fixed string concat operator switcheroo. Added tests.

--- src/Candidate.php
+++ src/Candidate.php
@@ -5,25 +5,25 @@ class Candidate
 {
     private $id;
     private $name;
-    
+
     public function __construct($id, $name)
     {
         $this->id = $id;
         $this->name = $name;
     }
-    
+
     public function getId()
     {
         return $this->id;
     }
-    
+
     public function getName()
     {
         return $this->name;
     }
-    
+
     public function __toString()
     {
-        return $this->name + "(" + $this->id + ")";
+        return $this->name . "(" . $this->id . ")";
     }
 }
