9d71b40b2f05cfd62a1a596da4202b62c1db25e8
Andrew Shell <andrew@andrewshell.org>
2017-07-03 15:01:00 -0500

    Build tools (#11)
    ### Added
    - `.editorconfig` file
    - `.gitattributes` file
    - `composer.lock` to `.gitignore`
    - `phpcs.xml` config
    - Bootstrap file for PHPUnit tests.

---
+++ tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+date_default_timezone_set('UTC');
+$loader = @include __DIR__ . '/../vendor/autoload.php';
+
+if (false === $loader) {
+    die(<<<'EOT'
+You must set up the project dependencies by running the following commands:
+
+curl -s http://getcomposer.org/installer | php
+php composer.phar install
+
+EOT
+    );
+}
