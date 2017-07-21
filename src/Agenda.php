8d5d3fea67d4246cfc29114e24e74b4b2da300c9
Carl Schroedl <carlschroedl@gmail.com>
2017-07-21 18:00:18 -0500

    Broken checkpoint

---
+++ src/Agenda.php
@@ -0,0 +1,31 @@
+<?php
+namespace PivotLibre\Tideman;
+use PivotLibre\Tideman\MarginRegistry;
+
+class Agenda
+{
+    private $candidateSet;
+    public function __construct(Ballot ...$ballots)
+    {
+        /**
+         * @todo #7 populate $candidateSet with all of the Candidates in $BallotTest
+         */
+
+
+         $this->candidateSet = new \SplObjectStorage();
+         echo $ballots;
+         //might be an array, or might be SplObjectStorage
+         //read this article, then decide
+         // http://technosophos.com/2009/05/29/set-objects-php-arrays-vs-splobjectstorage.html
+    }
+
+    public function getCandidates() : CandidateList
+    {
+
+         $candidateList = new CandidateList();
+         /**
+          * @todo #7 iterate through all of the candidates in the set, return a CandidateList;
+          */
+         return $candidateList;
+    }
+}
--- src/MarginCalculator.php
+++ src/MarginCalculator.php
@@ -1,9 +1,68 @@
 <?php
 namespace PivotLibre\Tideman;
+use PivotLibre\Tideman\MarginRegistry;
 
 class MarginCalculator
 {
     public function __construct()
     {
+        /**
+         * @todo #7 Decide whether the NBallots should be passed to the calculator
+         * at instantiation, or when invoking calculate().
+         */
     }
+
+    protected function initializeRegistry(Agenda $agenda) : MarginRegistry
+    {
+        $registry = new MarginRegistry();
+        /**
+         * @todo #7 register all possible pairs of candidates as a margin within the margin registry
+         */
+        return $registry;
+    }
+
+    protected function getCandidateIdToRankMap(Ballot $ballot) : array
+    {
+        $candidateIdToRank = array();
+        /**
+         * @todo #7 build map of candidate id to rank
+         */
+
+         //for each of the NBallot's candidate lists with counter i
+             //for each Candidate in each Candidate list,
+                 //ensure candidate's id is defined and non-empty
+                 //$candidateToRank[$candidate->getId()] = i;
+        return $candidateIdToRank;
+    }
+    public function calculate(Agenda $agenda, NBallot ...$nBallots) : MarginRegistry
+    {
+        $registry = $this->initializeRegistry($agenda);
+
+        foreach($nBallots as $nBallot)
+        {
+            $candidateIdToRank = $this->getCandidateIdToRankMap($nBallot); //a map of candidate id to their integer rank within an NBallot.
+
+            //for each candidate $outerCandidate in the agenda
+                //for each candidate $innerCandidate in the agenda
+                    //if $outerCandidate != $innerCandidate
+                        //$outerCandidateRank = $candidateToRank[$outerCandidate->getId()];
+                        //$innerCandidateRank = $candidateToRank[$innerCandidate->getId()];
+                        // $marginToUpdate;
+                        // $updateAmount = $nBallot->getCount();
+
+                        //if($outerCandidateRank > $innerCandidateRank) {
+                            //$marginToUpdate = $marginRegistry($outerCandidate->getId(), $innerCandidate->getId());
+                        // } else {
+                            //no need to explicitly handle special case $outerCandidateRank == $innerCandidateRank
+                            //One margin for the tied pair will be populate on the first pass through, the other margin will be populated
+                            //on the second pass through
+                            //$marginToUpdate = $marginRegistry($innerCandidate->getId(), $outerCandidate->getId());
+                        // }
+                        //
+            }
+        return $registry;
+    }
+
+
+
 }
