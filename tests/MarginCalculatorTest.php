9eebdd098edb2406bcfe20504901bb885940c5fd
Carl Schroedl <carlschroedl@gmail.com>
2017-07-22 16:57:40 -0500

    Added skeleton test class for MarginCalculator.

---
+++ tests/MarginCalculatorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace PivotLibre\Tideman;
+
+use PHPUnit\Framework\TestCase;
+
+class MarginCalculatorTest extends TestCase
+{
+    public function testMarginCalculator() : void
+    {
+        echo "Implement real test!";
+        $this->assertTrue(true);
+    }
+}
