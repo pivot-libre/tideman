a9cf00455a958b325cb93bc2989b0a3a9093a5f4
Carl Schroedl <carlschroedl@gmail.com>
2017-07-23 15:37:07 -0500

    Improving NBallot documentation.

--- src/NBallot.php
+++ src/NBallot.php
@@ -1,6 +1,11 @@
 <?php
 namespace PivotLibre\Tideman;
 
+/**
+ * This class shows how many times the same Ballot was submitted in an election.
+ * This object could be populated by the result of a COUNT  SQL query like:
+ * SELECT COUNT(ranking), ranking FROM ballots WHERE ranking = ...
+ */
 class NBallot extends Ballot
 {
     private $count;
