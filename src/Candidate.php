fad45c5b914cc97175d5c8bfca951bed8b9c7bd8
Carl Schroedl <carlschroedl@gmail.com>
2017-07-24 17:51:11 -0500

    Driving Candidate ID requirement into the Candidate class.

--- src/Candidate.php
+++ src/Candidate.php
@@ -1,28 +1,33 @@
 <?php
 namespace PivotLibre\Tideman;
 
+use \InvalidArgumentException;
+
 class Candidate
 {
     private $id;
     private $name;
 
-    public function __construct($id, $name)
+    public function __construct(string $id, string $name)
     {
+        if (empty($id) || empty(trim($id))) {
+            throw new InvalidArgumentException("A Candidate must have an id");
+        }
         $this->id = $id;
         $this->name = $name;
     }
 
-    public function getId()
+    public function getId() : string
     {
         return $this->id;
     }
 
-    public function getName()
+    public function getName() : string
     {
         return $this->name;
     }
 
-    public function __toString()
+    public function __toString() : string
     {
         return $this->name . "(" . $this->id . ")";
     }
