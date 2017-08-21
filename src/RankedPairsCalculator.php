cdbf6175cee601e72f0d625c3cf040e4c81fe4cc
Carl Schroedl <carlschroedl@gmail.com>
2017-08-20 19:26:17 -0500

    Improved clarity of the RankedPairsCalculator class.

--- src/RankedPairsCalculator.php
+++ src/RankedPairsCalculator.php
@@ -3,6 +3,9 @@
 namespace PivotLibre\Tideman;
 
 use \InvalidArgumentException;
+use \Exception;
+use PivotLibre\Tideman\MarginList;
+use PivotLibre\Tideman\CandidateList;
 use PivotLibre\Tideman\CandidateComparator;
 use PivotLibre\Tideman\TieBreakingMarginComparator;
 
@@ -12,6 +15,8 @@ class RankedPairsCalculator
 
     /**
      * Constructs a Ranked Pairs Calculator, verifying that the specified tie-breaking ballot contains no ties.
+     * Retains a copy of the tie-breaking Ballot so that the caller may modify the parameterized Ballot without
+     * affecting this class.
      * @param tieBreakingBallot
      */
     public function __construct(Ballot $tieBreakingBallot)
@@ -19,7 +24,7 @@ class RankedPairsCalculator
         if ($tieBreakingBallot->containsTies()) {
             throw new InvalidArgumentException("Tie breaking ballot must not contain any ties. $tieBreakingBallot");
         } else {
-            $this->tieBreakingBallot = $tieBreakingBallot;
+            $this->tieBreakingBallot = clone $tieBreakingBallot;
         }
     }
 
@@ -29,17 +34,50 @@ class RankedPairsCalculator
      */
     public function calculate(NBallot ...$nBallots) : CandidateList
     {
+        $marginList = $this->getMargins(...$nBallots);
+        $sortedMarginList = $this->sortMargins($marginList);
+        $rankedCandidates = $this->rankCandidates($sortedMarginList);
+
+    }
+
+    /**
+     * Tallies the Margins and returns the Margins with difference properties >= 0
+     */
+    public function getMargins(NBallot ...$nBallots) : MarginList
+    {
         $marginCalculator = new MarginCalculator();
         $marginRegistry = $marginCalculator->calculate(...$nBallots);
         $allMargins = $marginRegistry->getAll();
-        // $marginGroupsSortedDescByDifference = $allMargins->filterGroupAndSort()
-        // $marginsWithoutTies = $this->breakTies($marginGroupsSortedDescByDifference);
         $positiveOrZeroMargins = array_filter($allMargins->toArray(), function (Margin $margin) {
             return $margin->getDifference() >= 0;
         });
+        $marginList = new MarginList(...$positiveOrZeroMargins);
+        return $marginList;
+    }
 
+    /**
+     * Sorts all Margins in order of descending getDifference(). When Margins have the same difference property, ties
+     * are broken according to Tideman and Zavist's 1989 "Complete Independence of Clones" rule using the Ballot passed
+     * to this instance's constructor.
+     */
+    public function sortMargins(MarginList $marginList) : MarginList
+    {
         $tieBreaker = new TotallyOrderedBallotMarginTieBreaker(new CandidateComparator($this->tieBreakingBallot));
         $tieBreakingMarginComparator = new TieBreakingMarginComparator($tieBreaker);
         $sortedMargins = usort($positiveOrZeroMargins, $tieBreakingMarginComparator);
+        $sortedMarginList = new MarginList(...$sortedMargins);
+        return $sortedMargins;
+    }
+
+    /**
+     * Locks in Margins in order of descending difference, ignoring any Margins that would contradict a
+     * previously-locked-in Margin.
+     * @param MarginList a MarginList whose Margin's are sorted in order of descending difference.
+     * @return CandidateList - a list of Candidates in descending order of preference. Candidates that are more
+     * preferred have a lower index than Candidates that are less preferred.
+     */
+    public function rankCandidates(MarginList $sortedMarginList) : CandidateList
+    {
+        throw new Exception("Not implemented yet");
     }
 }
