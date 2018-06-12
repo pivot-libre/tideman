84be7f528428d811e1c4bb8f223f2988c3ec4105
Carl Schroedl <carlschroedl@gmail.com>
2018-06-11 20:51:09 -0500

    Made ballot parsing more robust, added some tests.

---
+++ tests/BallotParserTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace PivotLibre\Tideman;
+
+use PHPUnit\Framework\TestCase;
+
+
+class BallotParserTest extends TestCase
+{
+    private $instance;
+    private $alice;
+    private $bob;
+    private $claire;
+
+    public function setUp() : void
+    {
+        $this->instance = new BallotParser();
+        $this->alice = new Candidate("A");
+        $this->bob = new Candidate("B");
+        $this->claire = new Candidate("C");
+    }
+
+    public function testParseEmptyString() : void
+    {
+        $expected = new Ballot();
+        $actual = $this->instance->parse("");
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testParseEasyBallot() : void
+    {
+        $expected = new Ballot(
+            new CandidateList($this->alice),
+            new CandidateList($this->bob)
+        );
+        $actual = $this->instance->parse("A>B");
+        $this->assertEquals($expected, $actual);
+        $actual = $this->instance->parse("B<A");
+        $this->assertEquals($expected, $actual);
+
+
+        //ensure spaces are ignored
+        $actual = $this->instance->parse("A >B");
+        $this->assertEquals($expected, $actual);
+        $actual = $this->instance->parse("B <A");
+        $this->assertEquals($expected, $actual);
+
+        $actual = $this->instance->parse("A > B");
+        $this->assertEquals($expected, $actual);
+        $actual = $this->instance->parse("B < A");
+        $this->assertEquals($expected, $actual);
+
+        $actual = $this->instance->parse(" A  >   B    ");
+        $this->assertEquals($expected, $actual);
+        $actual = $this->instance->parse("    B   <  A ");
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testEnforceOneDirection() : void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->instance->parse("A<B>C");
+    }
+
+}
