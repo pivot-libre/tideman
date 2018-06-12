385b22e7d51c95e8b83c70a02bdcd56b29e6d2f3
Carl Schroedl <carlschroedl@gmail.com>
2018-06-11 20:51:09 -0500

    Made ballot parsing more robust, added some tests.

--- src/BallotParser.php
+++ src/BallotParser.php
@@ -2,21 +2,30 @@
 
 
 namespace PivotLibre\Tideman;
-
+use \InvalidArgumentException;
 
 class BallotParser
 {
-    private static const ORDERED_DELIM = "<>";
-    private static const EQUAL_DELIM = "=";
+    private const ORDERED_DELIM = "<>";
+    private const EQUAL_DELIM = "=";
 
+    /**
+     * @param string $text
+     *
+     * @return Ballot
+     */
     public function parse(string $text) : Ballot
     {
+        $this->enforceOneDirection($text);
         $listOfCandidateLists = [];
-        $orderedTokens = $this->tokenize(ORDERED_DELIM);
-        foreach($orderedTokens as $orderedToken) {
-            $equallyPreferredTokens = $this->tokenize($orderedToken, EQUAL_DELIM);
+        $orderedTokens = $this->tokenize($text, self::ORDERED_DELIM);
+        if ($this->contains("<", $text)) {
+            $orderedTokens = array_reverse($orderedTokens);
+        }
+        foreach ($orderedTokens as $orderedToken) {
+            $equallyPreferredTokens = $this->tokenize($orderedToken, self::EQUAL_DELIM);
             $equallyPreferredCandidates = [];
-            foreach($equallyPreferredTokens as $equallyPreferredToken) {
+            foreach ($equallyPreferredTokens as $equallyPreferredToken) {
                 $candidate = new Candidate($equallyPreferredToken);
                 $equallyPreferredCandidates[] = $candidate;
             }
@@ -27,13 +36,39 @@ class BallotParser
         return $ballot;
     }
 
+    /**
+     * @param $text
+     * @throws \InvalidArgumentException if the ballot string contains both ">" and "<"
+     */
+    private function enforceOneDirection($text) : void
+    {
+        if (
+            $this->contains(">", $text)
+            &&
+            $this->contains("<", $text)
+        ) {
+            throw new InvalidArgumentException("Ballot contained both '>' and '<'. It should only contain one or the other");
+        }
+    }
+
+
+    /**
+     * @param $needle
+     * @param $haystack
+     * @return bool true if $needle is in $haystack, false otherwise.
+     */
+    private function contains($needle, $haystack)
+    {
+        return strpos($haystack, $needle) !== false;
+    }
+
     private function tokenize(string $toTokenize, string $delim) : array
     {
         $tokens = [];
         $token = strtok($toTokenize, $delim);
 
         while ($token !== false) {
-            $tokens[] = $token;
+            $tokens[] = trim($token);
             $token = strtok($delim);
         }
         return $tokens;
