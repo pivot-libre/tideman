1c2abb891349b2bbea7b21966c7995cfcd9e8485
Carl Schroedl <carlschroedl@gmail.com>
2018-06-18 21:31:19 -0500

    Enforced style standard compliance. Added bin script to run class.

---
+++ bin/bulk_ballot_test.php
@@ -0,0 +1,12 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+
+// To use composer's autoload
+require_once '../vendor/autoload.php';
+
+use PivotLibre\Tideman\Tools\BulkBallotLoader;
+
+BulkBallotLoader::main();
