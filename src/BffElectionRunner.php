d497d2a6c675e73305713eca442a1a7328277343
Carl Schroedl <carlschroedl@gmail.com>
2018-10-01 05:20:18 -0500

    Made parsing more robust and improved testing

--- src/BffElectionRunner.php
+++ src/BffElectionRunner.php
@@ -10,6 +10,7 @@ class BffElectionRunner
     public function __construct()
     {
         $this->ballotParser = new BallotParser();
+        $this->nBallotParser = new NBallotParser();
         $this->candidateRankingSerializer = new CandidateRankingSerializer();
     }
 
@@ -24,23 +25,39 @@ class BffElectionRunner
 
     /**
      * @param string newline-delimited ballots formatted according to BFF
+     * @return string BFF-encoded result
      */
     public function run(string $bffBallots) : string
     {
-        //parse ballots
-        $splitBffBallots = preg_split("/\r\n|\n|\r/", $bffBallots);
-        $ballots = array_map(function (string $bffBallot) {
-            return $this->ballotParser->parse($bffBallot);
-        }, $splitBffBallots);
-        
-        //run election
+        $ballots = $this->parseMultiBallotString($bffBallots);
         $calculator = new RankedPairsCalculator($this->tieBreaker);
         $results = $calculator->calculate(count($ballots), ...$ballots);
-
-        //get ranking and convert to BFF string
         $ranking = $results->getRanking();
         $bffRanking = $this->candidateRankingSerializer->serialize($ranking);
 
         return $bffRanking;
+    }
+
+    /**
+     * @param string newline-delimited ballots formatted according to BFF
+     * @return array of NBallots
+     */
+    public function parseMultiBallotString($bffString) : array
+    {
+        //parse ballots
+
+        //input could come from many different OSs, so must support splitting on all common newline representations
+        $splitBffBallots = preg_split("/\r\n|\n|\r/", $bffString);
+        //trim each line
+        $splitBffBallots = array_map("trim", $splitBffBallots);
+        //filter out blank lines
+        $splitBffBallots = array_filter($splitBffBallots, function($line) {
+            return '' !== $line;
+        });
+        //parse each line
+        $ballots = array_map(function (string $bffBallot) {
+            return $this->nBallotParser->parse($bffBallot);
+        }, $splitBffBallots);
+        return $ballots;
     }
 }
--- tests/BffElectionRunnerTest.php
+++ tests/BffElectionRunnerTest.php
@@ -13,10 +13,59 @@ class BffElectionRunnerTest extends TestCase
         $this->instance = new BffElectionRunner();
     }
 
-    public function testSimpleElection() : void
+    public function testUnixLikeLineEndings() : void
     {
-        $this->instance->setTieBreaker('A>B>C');
+        $this->instance->setTieBreaker("A>B>C");
         $actual = $this->instance->run("A>B>C\nA>C>B");
+        $expected = 'A>B=C';
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testOldMacOsLineEndings() : void
+    {
+        $this->instance->setTieBreaker("A>B>C");
+        $actual = $this->instance->run("A>B>C\rA>C>B");
+        $expected = 'A>B=C';
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testWindowsLineEndings() : void
+    {
+        $this->instance->setTieBreaker("A>B>C");
+        $actual = $this->instance->run("A>B>C\r\nA>C>B");
+        $expected = 'A>B=C';
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testEmptyLinesIgnored() : void
+    {
+        $this->instance->setTieBreaker("\n\nA>B>C\n\n");
+        $actual = $this->instance->run("\n\nA>B>C\n\nA>C>B\n\n");
+        $expected = 'A>B=C';
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testThatLinesWithOnlyWhitespaceAreIgnored() : void
+    {
+        $this->instance->setTieBreaker("A>B>C");
+        $actual = $this->instance->run("\n \nA>B>C\n\t\nA>C>B\n \t \n");
+        $expected = 'A>B=C';
+        $this->assertEquals($expected, $actual);
+    }
+
+
+    public function testLeadingWhitespaceIgnored() : void
+    {
+        $this->instance->setTieBreaker(" \t A>B>C");
+        $actual = $this->instance->run(" \t A>B>C\n\t\tA>C>B");
+        $expected = 'A>B=C';
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testTrailingWhitespaceIgnored() : void
+    {
+        $this->instance->setTieBreaker("A>B>C \t ");
+        $actual = $this->instance->run("A>B>C\t\t\nA>C>B \t ");
         $expected = 'A>B=C';
         $this->assertEquals($expected, $actual);
     }
